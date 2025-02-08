f31495d5ede69084f80dd57070b9cfc1fe993b08
ANSHIKA GUPTA <anshikag.2118@gmail.com>
2025-02-08 21:04:48 +0530

    add reusable components: Button, Container, Input, Logo, and Logout button; enhance Header and Footer with navigation and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import Container from "../Container/Container";
+import { Logo, LogoutBtn } from "..";
 export default function Header() {
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add  Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="100px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 rounded-full hover:bg-blue-100"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
 }
